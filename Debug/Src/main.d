2f6b8837a390defb473bef79cb70d50da26cb13f
Bùi Trịnh Thế Viên <127777015+thevien257@users.noreply.github.com>
2025-06-03 17:09:15 +0700

    feat: update debug folder

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h503xx.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/core_cm33.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/mpu_armv8.h \
+ C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Device/ST/STM32H5xx/Include/system_stm32h5xx.h
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h503xx.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/core_cm33.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Include/mpu_armv8.h:
+C:/Users/ADMIN/Desktop/CanRX/stm32h503-can-protocols/Drivers/CMSIS/Device/ST/STM32H5xx/Include/system_stm32h5xx.h:
---
+++ Debug/Startup/startup_stm32h503cbux.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32h503cbux.o: ../Startup/startup_stm32h503cbux.s
